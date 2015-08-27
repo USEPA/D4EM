fb3c53a9d0f6344628417860a4df5f591a89cd34
Mark Gray <MarkGrayATC@users.noreply.github.com>
2015-08-27 14:37:21 -0400

    modPublishSWAT.vb   GetSwatInputFiles: include all files that are not output files instead of focusing on only two kinds of input files

--- WEDO.Publish/modPublishSWAT.vb
+++ WEDO.Publish/modPublishSWAT.vb
@@ -8,8 +8,12 @@ Module modPublishSWAT
     Public Function GetSwatInputFiles() As List(Of String)
         Dim lInputFiles As New List(Of String)
         For Each lFileName As String In IO.Directory.GetFiles(IO.Path.GetDirectoryName(CioFilename))
-            Select Case IO.Path.GetExtension(lFileName).ToLowerInvariant()
-                Case ".pcp", ".tmp" ' "output.rch", "output.sub", "output.hru", "output.hrux"
+            Select Case IO.Path.GetFileName(lFileName).ToLowerInvariant()
+                Case "output.rch", "output.sub", "output.hru", "output.hrux",
+                     "hyd.out", "output.sed", "output.std", "tempvel.out",
+                     "watout.dat", "watqual.dat", "chan.deg", "hru.dat", "input.std", "output.rsv", "output.wtr", "rch.dat", "rsv.dat", "sub.dat", "fort.1112"
+                    'Skip output files
+                Case Else
                     lInputFiles.Add(lFileName)
             End Select
         Next
