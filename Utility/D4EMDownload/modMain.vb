f3d2d49aad1d56423f9dfd9b5f293dde206db6d3
Mark Gray <MarkGrayATC@users.noreply.github.com>
2015-09-14 14:19:03 -0400

    New program that will use D4EM libraries to download data. Initially designed for use by BASINS, could also be used by other programs.

---
+++ Utility/D4EMDownload/modMain.vb
@@ -0,0 +1,10 @@
+﻿Module modMain
+    Sub Main()
+        If My.Application.CommandLineArgs.Count > 0 Then
+            Dim lInstructionsFilename As String = My.Application.CommandLineArgs(0)
+            D4EM.Data.Globals.Initialize()
+        Else
+            MsgBox("Command line was empty, expected XML instruction file name.")
+        End If
+    End Sub
+End Module
